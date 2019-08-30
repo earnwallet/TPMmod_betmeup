a2466221dbfeed887f6ff47d352fb76aeaa8aadb
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 21:09:33 +0200

    Create settings for martingale method

---
+++ method/martingale/settings.php
@@ -0,0 +1,17 @@
+<?php
+// Minimal balance needed for this script to work.
+// Don't forget about withdrawal fee.
+$MinBalance     = 25;
+
+// What balance percent to use?
+// MineBalance must be higher than Balance percent to script to start, otherwise it will be skipped. 
+$BalancePercent = 10;
+
+// Limit bankroll.
+$MaxBalance     = 50;
+
+// use scale 1-10 plz
+$risk           = 9;
+
+// Make this amount of profit (%) +2Doge fee (deposit + withdrawal)
+$profit         = "10";
