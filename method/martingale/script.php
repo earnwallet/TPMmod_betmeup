05272967cc971caccec4fbfb8f9532d2cbbb7393
Cyjan <MrCyjaneK@Gmail.com>
2019-08-31 13:22:11 +0200

    Update script.php
    - withdraw logic added
    - bugfix

--- method/martingale/script.php
+++ method/martingale/script.php
@@ -1,5 +1,14 @@
 <?php
 include "./settings.php";
+/* 
+ * > settings.php
+ * $MinBalance
+ * $BalancePercent
+ * $MaxBalance
+ * $risk
+ * $profit
+*/ 
+
 $filename = $argv[0];
 $host     = $argv[1];
 $port     = $argv[2];
@@ -7,14 +16,22 @@ $port     = $argv[2];
 // xxx $argv[0]   $argv[1]  $argv[2]
 
 // Get balance;
+sleep(30);   // Let the wallet load
+// TODO: check if wallet is loaded instead of sleep
 $tpmbalance  = explode(PHP_EOL, shell_exec(`dogecoin-cli getbalance`))[0];
 $dicebalance = request("?action=balance");
+$profitneed  = round($tpmbalance*($BalancePercent/100)*(($profit/100)+1),4);
+$totalprofit = -2; // Fee for withdrawal+deposit
 $nextbet     = 0;
 $chance      = 49.95;
 $bethigh     = false;
-sleep(60); //Let the deposit credit.
+sleep(60);   //Let the deposit credit.
 // TODO: check balance instead of sleep
 while (1) {
+    // Please don't use $totalprofit
+    // Withdrawal system depends on it.
+    $totalprofit -= $nextbet;
+    $totalprofit += $profit;
     // Define variables
     $resp        = placeBet($nextbet,$chance,$bethigh);
     try {
@@ -25,7 +42,7 @@ while (1) {
     }
     $previousbet   = $nextbet;
     $chance        = 49.95;
-    $profit        = $resp->win*10^8;
+    $profit        = $resp->win/10^8;
     $win           = 0;
     $balance       = $resp->balance;
     if ($resp->win == 0) {
@@ -48,6 +65,10 @@ while (1) {
     }
     // Do betting here
 
+
+
+
+
     $chance       = 49.95;
     $base         = $balance/1000000;
     if ($base < 0.00000001) {
@@ -59,10 +80,17 @@ while (1) {
         $nextbet = $previousbet*2;
     }
 
+
+
+
+
     // Do not betting here
     shell_exec(
-        'echo "[`date`] (bet: '.round($previousbet*10^8,8).') [win: '.$win.']" >> "/home/`whoami`/TPM/mods/betmeup/debug.log"'
+        'echo "[`date`] (bet: '.round($previousbet*10^8,8).') [win: '.$win.'] [profit: '.$totalprofit.'] ['.round($profitneed/$totalprofit/100,2).'%] >> "/home/`whoami`/TPM/mods/betmeup/debug.log"'
     );
+    if ($totalprofit > $profitneed) {
+        request("?action=withdraw&amount=0&address=".explode(PHP_EOL,shell_exec(`dogecoin-cli getnewaddress`)[0]);
+    }
     // sleep for 0.1 second.
     usleep(100000);
 }
@@ -77,5 +105,5 @@ function request($wat) {
 function placeBet($amount = 0, $chance = 49.95, $bethigh = true) {
     $amt      = round($amount*10^8); // make it dogetoshi
     $chance   = round($chance,2);    
-    return request("/?action=bet&amount=$amt&chance=$chance&bethi=$bethigh");
+    return request("?action=bet&amount=$amt&chance=$chance&bethi=$bethigh");
 }
