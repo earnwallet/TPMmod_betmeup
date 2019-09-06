58dc090be98a3fcce0b3c8f9e50a0eaf557e4203
mrcyjanek <mrcyjanek@gmail.com>
2019-09-06 20:42:24 +0200

    Bugfix

--- startscript.php
+++ startscript.php
@@ -14,7 +14,7 @@ include "/home/$username/TPM/mods/betmeup/method/$method/settings.php";
  * $risk
  * $profit
 */
-//$balance = explode(PHP_EOL, shell_exec("dogecoin-cli getbalance"))[0];
+$balance = explode(PHP_EOL, shell_exec("dogecoin-cli getbalance"))[0];
 //$depoadd = file_get_contents(
 //        "http://".$argv[2].":".$argv[3]."/?action=deposit"
 //    );
