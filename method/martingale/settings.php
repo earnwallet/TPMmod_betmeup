31f5140d4905257604fd92d03f50b5c7ce2cec0e
mrcyjanek <mrcyjanek@gmail.com>
2019-09-07 07:29:07 +0200

    What... in name of f...

--- method/martingale/settings.php
+++ method/martingale/settings.php
@@ -1,17 +1,20 @@
 <?php
+// ScriptName should be same as subdirectory
+$scriptname     = 'martingale';
+
 // Minimal balance needed for this script to work.
 // Don't forget about withdrawal fee.
 $MinBalance     = 25;
 
 // What balance percent to use?
 // MineBalance must be higher than Balance percent to script to start, otherwise it will be skipped. 
-$BalancePercent = 10;
+$BalancePercent = 5;
 
 // Limit bankroll.
 $MaxBalance     = 50;
 
 // use scale 1-10 plz
-$risk           = 9;
+$risk           = 10;
 
-// Make this amount of profit (%) +2Doge fee (deposit + withdrawal)
-$profit         = "10";
+// Make this amount of profit (%)
+$profit         = 10;
