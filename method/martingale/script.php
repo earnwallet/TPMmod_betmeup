82164b4112548bb774355a9a52d61d6dc1417895
mrcyjanek <mrcyjanek@gmail.com>
2019-09-07 07:27:10 +0200

    Make code more readable

--- method/martingale/script.php
+++ method/martingale/script.php
@@ -1,8 +1,92 @@
 <?php
+<<<<<<< HEAD
 include './vars.php';
 include './init.php';
 while (1) {
     include './loopbegin.php';
+=======
+include "./settings.php";
+/* 
+ * > settings.php
+ * $MinBalance
+ * $BalancePercent
+ * $MaxBalance
+ * $risk
+ * $profit
+*/ 
+
+$filename = $argv[0];
+$host     = $argv[1];
+$port     = $argv[2];
+// php script.php 127.0.0.1 42999
+// xxx $argv[0]   $argv[1]  $argv[2]
+
+
+// Get balance;
+echo "\nWaiting for wallet to load\n";
+sleep(30);   // Let the wallet load
+// TODO: check if wallet is loaded instead of sleep
+$tpmbalance  = explode(PHP_EOL, shell_exec("dogecoin-cli getbalance"))[0];
+echo "\nWallet balance  : $tpmbalance";
+$dicebalance = request("?action=balance");
+echo "\nSite balance    : $dicebalance";
+$profitneed  = round($tpmbalance*($BalancePercent/100)*(($profit/100)),4);
+echo "\nProfit needed   : $profitneed";
+$deponeed    = round($tpmbalance*($BalancePercent/100));
+echo "\nDeposit amount  : $deponeed";
+$totalprofit = 0;
+$nextbet     = 0;
+$chance      = 49.95;
+$bethigh     = false;
+$betcount    = 0;
+$profit      = 0;
+echo "\nSending deposit...";
+ echo shell_exec("dogecoin-cli sendtoaddress \"".request('?action=deposit')."\" \"$deponeed\"");
+echo "\nWaiting for deposit to credit...";
+sleep(60);   //Let the deposit credit.
+// TODO: check balance instead of sleep
+while (1) {
+    // Please don't use $totalprofit
+    // Withdrawal system depends on it.
+    //echo "\nTP: $totalprofit | TG: $profitneed";
+    //echo "\nNB: $nextbet";
+    //echo "\nBP: $profit";
+    $totalprofit -= $nextbet;
+    $totalprofit += $profit;
+    // Define variables
+    $resp        = placeBet($nextbet,$chance,$bethigh);
+    try {
+        $resp    = json_decode($resp);
+	print_r($resp);
+    } catch ( Exception $e ) {
+        print_r($e);
+        sleep(5);
+    }
+    $previousbet   = $nextbet;
+    $chance        = 49.95;
+    $profit        = round($resp->win/100000000,8);
+    echo "\nprofit: $profit";
+    $win           = 0;
+    $balance       = $resp->balance/100000000;
+    if ($resp->win == 0) {
+        $win       = 0;
+    } else {
+        $win       = 1;
+    }
+    $betcount++;
+    $currentstreak = 0;
+    if ($win) {
+        if ($currentstreak < 0) {
+            $currentstreak = 0;
+        }
+        $currentstreak++;
+    } else {
+        if ($currentstreak > 0) {
+            $currentstreak = 0;
+        }
+        $currentstreak--;
+    }
+>>>>>>> parent of 4547b2c... Fixed martingale, now it's 100% working
     // Do betting here
     $chance       = 49.95;
     $base         = $balance/1000000;
@@ -15,5 +99,40 @@ while (1) {
         $nextbet = $previousbet*2;
     }
     // Do not betting here
+<<<<<<< HEAD
     include './loopend.php';
+=======
+    shell_exec(
+        'echo "[`date`] (bet: '.round($previousbet*100000000,8).') [win: '.$win.'] [profit: '.$totalprofit.'] ['.round($totalprofit/$profitneed/100,2).'%]" >> "/home/`whoami`/TPM/mods/betmeup/debug.log"'
+    );
+    if ($totalprofit > $profitneed) {
+	echo "\ntotal profit: $totalprofit";
+	echo "\nneeded      : $profitneed";
+	sleep(25);
+	echo "\nWithdrawing all balance to TPM";
+        shell_exec(
+        'echo "[`date`] Withdrawing all balance..." >> "/home/`whoami`/TPM/mods/betmeup/debug.log"'
+    );
+        request("?action=withdraw&amount=0&address=".explode(PHP_EOL,shell_exec("dogecoin-cli getnewaddress"))[0]);
+	exit(0);
+    }
+    // sleep for 0.05 second.
+    usleep(50000);
+}
+
+function request($wat) {
+    global $host;
+    global $port;
+    $server = "http://$host:$port/";
+    return file_get_contents($server.$wat);
+}
+
+function placeBet($amount = 0, $chance = 49.95, $bethigh = true) {
+    $amt      = round($amount*100000000); // make it dogetoshi
+    if ($amt < 1) {
+	$amt = 1;
+    }
+    $chance   = round($chance,2);    
+    return request("?action=bet&amount=$amt&chance=$chance&bethi=$bethigh");
+>>>>>>> parent of 4547b2c... Fixed martingale, now it's 100% working
 }
--- method/martingale/settings.php
+++ method/martingale/settings.php
@@ -1,20 +1,17 @@
 <?php
-// ScriptName should be same as subdirectory
-$scriptname     = 'martingale';
-
 // Minimal balance needed for this script to work.
 // Don't forget about withdrawal fee.
 $MinBalance     = 25;
 
 // What balance percent to use?
 // MineBalance must be higher than Balance percent to script to start, otherwise it will be skipped. 
-$BalancePercent = 5;
+$BalancePercent = 10;
 
 // Limit bankroll.
 $MaxBalance     = 50;
 
 // use scale 1-10 plz
-$risk           = 10;
+$risk           = 9;
 
-// Make this amount of profit (%)
-$profit         = 10;
+// Make this amount of profit (%) +2Doge fee (deposit + withdrawal)
+$profit         = "10";
